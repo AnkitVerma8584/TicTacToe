eaad81fa7a14293cdb0918494beb6905b004475a
AnkitVerma8584 <av8584060@gmail.com>
2019-10-29 21:44:21 +0530

    A small tic tac game

---
+++ app/src/main/java/com/example/tictac/MainActivity.java
@@ -0,0 +1,174 @@
+package com.example.tictac;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btn[][]=new Button[3][3];
+    Button rst;
+    TextView p1,p2;
+    boolean p1turn=true,p2turn;
+    int i,j,round=0,pl1,pl2,c=1;
+    String player1name,player2name,p1name,p2name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rst=findViewById(R.id.reset);
+        p1=findViewById(R.id.p1);
+        p2=findViewById(R.id.p2);
+        player1name=getIntent().getExtras().getString("Player1");
+        player2name=getIntent().getExtras().getString("Player2");
+        p1name=player1name+" : 0";
+        p2name=player2name+" : 0";
+        p1.setText(p1name);
+        p2.setText(p2name);
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                String btnid="btn"+i+j;
+                int resId=getResources().getIdentifier(btnid,"id",getPackageName());
+                btn[i][j]=findViewById(resId);
+                btn[i][j].setOnClickListener(this);
+            }
+        }
+        rst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset();
+                p1.setText(p1name);
+                pl1=0;
+                p2.setText(p2name);
+                pl2=0;
+                c=1;
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(((Button) v).getText().toString().equals("")) {
+            if (p1turn)
+            {
+                ((Button) v).setText("X");
+
+            }
+            else
+            {
+                ((Button) v).setText("O");
+
+            }
+            p1turn=p2turn;
+            p2turn=!p2turn;
+            round++;
+        }else return;
+        if(Whowin().equals("X"))
+        {
+            Toast.makeText(getApplicationContext(),player1name+ " wins",Toast.LENGTH_LONG).show();
+            reset();
+            change();
+            pl1++;
+            String a=p1.getText().toString();
+            a=a.substring(0,a.indexOf(':'))+": "+pl1;
+            p1.setText(a);
+
+        }
+        if(Whowin().equals("O"))
+        {
+            Toast.makeText(getApplicationContext(),player2name+" wins",Toast.LENGTH_LONG).show();
+            reset();
+            change();
+            pl2++;
+            String a=p2.getText().toString();
+            a=a.substring(0,a.indexOf(':'))+": "+pl2;
+            p2.setText(a);
+
+        }
+        if(round==9&&Whowin().equals("No"))
+        {
+            Toast.makeText(getApplicationContext(),"DRAW",Toast.LENGTH_LONG).show();
+            reset();
+        }
+    }
+
+    private void reset() {
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++)
+                btn[i][j].setText("");
+        }
+        p1turn=true;
+        p2turn=false;
+        round=0;
+    }
+
+    private String Whowin() {
+        int i,j;
+        String ch[][]=new String[3][3];
+        for(i=0;i<3;i++) {
+            for (j = 0; j < 3; j++)
+                ch[i][j] = btn[i][j].getText().toString();
+        }
+
+        for (j = 0; j < 3; j++)
+        {   if((ch[j][0].equals(ch[j][1]))&&(ch[j][0].equals(ch[j][2])))
+                    return ch[j][0];
+        }
+        for (j = 0; j < 3; j++)
+        {   if((ch[0][j].equals(ch[1][j]))&&(ch[0][j].equals(ch[2][j])))
+                return ch[0][j];
+
+        }
+        if(((ch[0][0].equals(ch[1][1]))&&(ch[0][0].equals(ch[2][2])))||(((ch[0][2].equals(ch[1][1]))&&(ch[0][2].equals(ch[2][0])))))
+            return ch[1][1];
+        return "No";
+    }
+    boolean f=false;
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder alt=new AlertDialog.Builder(this);
+        alt.setTitle("Alet!")
+                .setCancelable(false)
+                .setMessage("Are you sure you wanna quit")
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+        AlertDialog a=alt.create();
+                a.show();
+    }
+    public void change()
+    {
+        if(c==1) {
+            p1turn = false;
+            p2turn=true;
+        }
+        if(c==-1) {
+            p2turn = false;
+            p1turn=true;
+        }
+        if(p1turn)
+            Toast.makeText(getApplicationContext(), player1name+" will start", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(getApplicationContext(), player2name+" will start", Toast.LENGTH_LONG).show();
+        c=c*-1;
+
+    }
+}
---
+++ app/src/main/java/com/example/tictac/Start.java
@@ -0,0 +1,57 @@
+package com.example.tictac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Start extends AppCompatActivity {
+    Button btn;
+    EditText p1,p2;
+    String pl1,pl2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        btn=findViewById(R.id.play);
+        p1=findViewById(R.id.p1);
+        p2=findViewById(R.id.p2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pl1=p1.getText().toString().trim();
+                pl2=p2.getText().toString().trim();
+                if(pl1.equals("")||pl2.equals(""))
+                {
+                    Toast.makeText(getApplicationContext(),"Enter Player names",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Intent it=new Intent(Start.this,MainActivity.class);
+                    it.putExtra("Player1",pl1);
+                    it.putExtra("Player2",pl2);
+                    startActivity(it);
+                    finish();
+                }
+            }
+        });
+
+    }
+    boolean f=false;
+
+    @Override
+    public void onBackPressed() {
+        if(!f) {
+            f=true;
+            Toast.makeText(getApplicationContext(), "Press back again to exit.", Toast.LENGTH_LONG).show();
+        }
+        else
+            finish();
+
+    }
+}
